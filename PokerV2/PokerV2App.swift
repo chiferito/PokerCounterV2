4cf92e733657edb52869dcf6c9bf469a20133baa
chiferito <ericwongliu@hotmail.com>
2024-06-15 11:56:23 -0500

    Initial Commit

---
+++ PokerV2/PokerV2App.swift
@@ -0,0 +1,17 @@
+//
+//  PokerV2App.swift
+//  PokerV2
+//
+//  Created by Eric Wong Liu on 2024-06-15.
+//
+
+import SwiftUI
+
+@main
+struct PokerV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
