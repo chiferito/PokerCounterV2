5c7277d4ac4566a830367f8e88bccd19f35cecc1
chiferito <ericwongliu@hotmail.com>
2024-06-15 17:53:08 -0500

    V2 Poker

---
+++ PokerV2/GameInfo.swift
@@ -0,0 +1,8 @@
+//
+//  GameInfo.swift
+//  PokerV2
+//
+//  Created by Eric Wong Liu on 2024-06-15.
+//
+
+import Foundation
---
+++ PokerV2/Player.swift
@@ -0,0 +1,8 @@
+//
+//  Player.swift
+//  PokerV2
+//
+//  Created by Eric Wong Liu on 2024-06-15.
+//
+
+import Foundation
